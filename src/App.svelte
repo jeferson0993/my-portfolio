8823d9f62d15efce802b5be1bf493479e6b9f19a
Jeferson Ferreira <jeferson0993@gmail.com>
2020-01-10 11:02:12 -0300

    iniciado novo projeto para reescrita do meu portifolio

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<p><a href="https://svelte.dev/docs">Svelte doc</a>.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2.5em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
