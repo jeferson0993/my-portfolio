cdf1fe76026e76208414662e8a4a9534c38d9461
Jeferson Ferreira <jeferson0993@gmail.com>
2020-01-10 11:49:08 -0300

    importados os dados estaticos e adicionado primeiro componente filho ao projeto (NAVBAR)

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+	import DATA from "./data/data.js";
+	import Navbar from "./components/navbar/Navbar.svelte";
 	export let name;
 </script>
 
 <main>
+
+	<!-- Navbar -->
+	<Navbar navlists={DATA.NAVBAR_DATA} header={DATA.HEADER} />
+
 	<h1>{name}</h1>
 	<p><a href="https://svelte.dev/docs">Svelte doc</a>.</p>
+
 </main>
 
 <style>
@@ -16,7 +23,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #ff0000;
 		text-transform: uppercase;
 		font-size: 2.5em;
 		font-weight: 100;
