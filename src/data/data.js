6609b455f73791e3dad5d34b86162ccfd7684afa
Jeferson Ferreira <jeferson0993@gmail.com>
2020-01-10 11:45:57 -0300

    adicionado primeiros dados estaticos ao projeto

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+const HEADER = "Jeferson0993";
+
+const NAVBAR_DATA = [
+    { id: 1, url: "/", label: "Home" },
+    { id: 2, url: "#webdev", label: "Services on the Web" },
+    { id: 3, url: "#about", label: "About" },
+    { id: 4, url: "#mobile", label: "Services on the mobile" },
+    { id: 5, url: "#skills", label: "My Skills" },
+    { id: 6, url: "#footer", label: "Contacts" }
+];
+
+const MOCK_DATA = {
+    HEADER,
+    NAVBAR_DATA,
+};
+  
+export default MOCK_DATA;
